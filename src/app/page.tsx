f24f3558053636537f1c9cb75de3d14104ce6b07
Karen Félix <karen.felix.rocha@gmail.com>
2024-04-16 20:36:53 -0300

    refactor: change page background and title font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <h1>Olá, eu sou a Karen!</h1>
+    <main className="flex bg-zinc-900 min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <h1 className="text-gray-200 font-bold">Olá, eu sou a Karen!</h1>
       </div>
     </main>
   );
