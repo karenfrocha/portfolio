5456cef813019bf49d97d4d544213cf390d97de9
Karen Félix <karen.felix.rocha@gmail.com>
2024-04-16 20:37:38 -0300

    feat: add personal avatar on main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <main className="flex bg-zinc-900 min-h-screen flex-col items-center justify-between p-24">
       <div className="flex flex-col justify-center items-center gap-4">
         <h1 className="text-gray-200 font-bold">Olá, eu sou a Karen!</h1>
+        <Image
+          src="https://github.com/karenfrocha.png"
+          className="rounded-full"
+          width={200}
+          height={200}
+          alt="avatar"
+        />
       </div>
     </main>
   );
