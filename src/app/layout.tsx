8ce8d9cc009ac5b6a9d61588780d944b6b5de2f5
Karen Félix <karen.felix.rocha@gmail.com>
2024-04-16 21:01:00 -0300

    feat: add navigation bar on root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex flex-row justify-center items-center gap-8 h-10 bg-zinc-600 border-b-[0.05rem] border-gray-100 font-bold text-gray-300">
+          <Link href={"/"} className="hover:text-gray-900">
+            Início
+          </Link>
+          <Link href={"/contato"} className="hover:text-gray-900">
+            Contato
+          </Link>
+          <Link href={"/sobre-mim"} className="hover:text-gray-900">
+            Sobre mim
+          </Link>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
