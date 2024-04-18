b8e2b1dbc1a641266fa781bdb8f9ba96df362065
Karen Félix <karen.felix.rocha@gmail.com>
2024-04-17 22:41:06 -0300

    refactor: change in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,15 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="flex flex-row justify-center items-center gap-8 h-10 bg-zinc-600 border-b-[0.05rem] border-gray-100 font-bold text-gray-300">
+        <div className="flex flex-row justify-center items-center gap-8 h-10 bg-zinc-600 font-bold text-gray-300">
           <Link href={"/"} className="hover:text-gray-900">
             Início
           </Link>
-          <Link href={"/contato"} className="hover:text-gray-900">
-            Contato
-          </Link>
           <Link href={"/sobre-mim"} className="hover:text-gray-900">
-            Sobre mim
+            Um pouco sobre mim
           </Link>
         </div>
         {children}
